08ba0aa25679ccc0bcfc75b009c39b9c40cc9677
Gabriel Rüdger <60497549+gabrielrudgerr@users.noreply.github.com>
2020-07-30 14:50:51 -0300

    UC10 - Implementar listagem de clientes com Angular 2+ [Parte 4]

--- src/main/ui/src/app/customer-list/customer-list.component.ts
+++ src/main/ui/src/app/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-list',
@@ -7,27 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  customers = [
-{
-  nome: "Tony",
-  data: "14/11/01",
-  email: "tony@gmail.com"
-},
-{
-  nome: "Prussian",
-  data: "02/09/03",
-  email: "Prussian@gmail.com"
-},
-{
-  nome: "Leipzig",
-  data: "02/05/03",
-  email: null
-}
-];
+private customers: Customer[];
 
   constructor() { }
 
   ngOnInit() {
+    this.customers = [
+{
+  name: "Tony",
+  data: "14/11/01",
+  email: "tony@gmail.com"
+},
+{
+  name: "Prussian",
+  data: "02/09/03",
+  email: "Prussian@gmail.com"
+},
+{
+  name: "Leipzig",
+  data: "02/05/03",
+  email: "Leipzig@gmail.com"
+}
+];
   }
+  
+  onSubmit(customerForm: NgForm, customerName: String, customerData: String, customerEmail: String) {
+    if (customerForm.valid) {
+      let newCustomer = new Customer();
+      newCustomer.name = customerName;
+      newCustomer.data = customerData;
+      newCustomer.email = customerEmail;
+      this.customers.push(newCustomer)
+    }
+  }
+}
 
+export class Customer {
+  name: String;
+  data: String;
+  email: String;
 }
